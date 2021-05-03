62c16d0ba91c113273c70e1a77ad0ce1aa315f03
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-05-03 17:24:58 +0200

    cleaned default project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main">
+    <h1> TobiRollz </h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
